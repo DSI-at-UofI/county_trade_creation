43ecffefd681fd28135b3bf6e1241d291cac74a1
Noé J Nava <noejnava2@gmail.com>
2021-12-08 20:58:03 -0600

    Dec 8, 21: Changing the mundlak test based on average of main variables to dummies.

--- code/mundalk_like_test.R
+++ code/mundalk_like_test.R
@@ -1,26 +1,11 @@
 rm(list = ls())
 
-# Purpose: Here, I performed a mundlak-like test to check if some state flows
+# Purpose: Here, I performed a simple statistical test to check if some state flows
 # may have some unobserved influence that can be remove with unit fixed-effects
-# The problem is that we cannot use fixed-effects in because our gdp/sales variables
-# would loose information. But we want to check if there is some additional
-# variable (conditional on gdp/sales) that affect one direction of the flows
+# The problem is that we cannot use year importer/exporter FE because our gdp/sales 
+# variables would not be identified. But we want to check if there is some additional
+# variable (conditional on a fully specified model) that affect one direction of the flows
 # for each state.
-
-# I also show that including an indicator for the problematic observations
-# is the same as dropping the observation. 
-
-# A caveat: We know that we do not want to use importer/exporter FE since those
-# may affect our gdp/sales coefficients. However, dropping observations is 
-# as bad (or I would say worse) since we are not only loosing the information
-# for an appropiate covariate for gdp/sales, but we do not know what other info
-# we are loosing for say the distance measure.
-
-# My conclusion: I still think that we are better off by not dropping observation,
-# or including an indicator, but if we have to, I would use the indicator since
-# using an indicator is less intrusive,
-
-# Reference: https://blog.stata.com/2015/10/29/fixed-effects-or-random-effects-the-mundlak-approach/
 
 library(readstata13)
 library(tidyverse)
@@ -43,40 +28,38 @@ for(i in 1:18) {
 }
 formula <- paste0("trade ~ indicator + distance + sales_i + gdp_j + contiguity + intra", formula)
 
-# Show it will be (almost) the same if we treat "outliers" with indicators
-# From stata:
-## distance  == -.4495181
-## sales_i   == .6802411 
-## gdp_j     ==  .9762757
-## intercept == -18.11583 
-data$trade[is.na(data$trade)] <- 0
-indices <- (data$dest == "louisiana" | data$dest == "washington")
-indicator <- ifelse(indices, 1, 0)
-
-out <- glm(formula, family = "quasipoisson", data = data)
-summary(out)
-
 # The mundlak-like test for destination
 st_list <- data %>% select(orig) %>% distinct() %>% arrange(orig) %>% pull()
+
+mundalkOut_dest <- data.frame()
+mundalkOut_dest <- rbind.data.frame(mundalkOut_dest, rep(NA,6))
+mundalkOut_dest <- cbind.data.frame(st_list, mundalkOut_dest)
+names(mundalkOut_dest) <- c("state", 
+                            "coeff_12", "SE_12", "pval_12",
+                            "coeff_17", "SE_17", "pval_17")
+
 for(st in st_list) {
   for(year in c(2012, 2017)) {
-    
+
     indices <- (data$dest == st) & data$year == year
-    
-    mean_sales <- mean(data$sales_i, na.rm = TRUE)
-    mean_gdp_j <- mean(data$gdp_j, na.rm = TRUE)
-    
-    mean_sales <- mean_sales*indicator
-    out <- glm(formula, family = "quasipoisson", data = data)
     indicator <- ifelse(indices, 1, 0)
-    pval_sales <- summary(out)$coefficients[2,4] # Recover p-value
     
-    indicator <- mean_gdp_j*indicator
-    out <- glm(formula, family = "quasipoisson", data = data)
-    indicator <- ifelse(indices, 1, 0)
-    pval_gdp_j <- summary(out)$coefficients[2,4] # Recover p-value
+    out  <- glm(formula, family = "quasipoisson", data = data)
+    pval <- summary(out)$coefficients[2,4] # Recover p-value
+    coef <- summary(out)$coefficients[2,1] # Recover coefficient
+    se   <- summary(out)$coefficients[2,2] # Recover SE
     
-    if(pval_sales < 0.05 & pval_gdp_j < 0.05) {
+    if(year == 2012) {
+      mundalkOut_dest[mundalkOut_dest$state == st,][2:4] <- c(coef,
+                                                              se,
+                                                              pval)
+    }else{
+      mundalkOut_dest[mundalkOut_dest$state == st,][5:7] <- c(coef,
+                                                              se,
+                                                              pval)
+    }
+    
+    if(pval < 0.05) {
       cat("\n")
       cat(":::", st, "is significant for the year of", year)
       cat("\n")
@@ -85,29 +68,40 @@ for(st in st_list) {
 }
 
 # The mundlak-like test for origin
+mundalkOut_orig <- data.frame()
+mundalkOut_orig <- rbind.data.frame(mundalkOut_orig, rep(NA,6))
+mundalkOut_orig <- cbind.data.frame(st_list, mundalkOut_orig)
+names(mundalkOut_orig) <- c("state", 
+                            "coeff_12", "SE_12", "pval_12",
+                            "coeff_17", "SE_17", "pval_17")
 for(st in st_list) {
   for(year in c(2012, 2017)) {
     
     indices <- (data$orig == st) & data$year == year
-    
-    mean_sales <- mean(data$sales_i, na.rm = TRUE)
-    mean_gdp_j <- mean(data$gdp_j, na.rm = TRUE)
-    
-    mean_sales <- mean_sales*indicator
-    out <- glm(formula, family = "quasipoisson", data = data)
     indicator <- ifelse(indices, 1, 0)
-    pval_sales <- summary(out)$coefficients[2,4] # Recover p-value
+        
+    out   <- glm(formula, family = "quasipoisson", data = data)
+    pval  <- summary(out)$coefficients[2,4] # Recover p-value
+    coef  <- summary(out)$coefficients[2,1] # Recover coefficient
+    se    <- summary(out)$coefficients[2,2] # Recover SE
     
-    indicator <- mean_gdp_j*indicator
-    out <- glm(formula, family = "quasipoisson", data = data)
-    indicator <- ifelse(indices, 1, 0)
-    pval_gdp_j <- summary(out)$coefficients[2,4] # Recover p-value
+    if(year == 2012) {
+      mundalkOut_orig[mundalkOut_orig$state == st,][2:4] <- c(coef,
+                                                              se,
+                                                              pval)
+    }else{
+      mundalkOut_orig[mundalkOut_orig$state == st,][5:7] <- c(coef,
+                                                              se,
+                                                              pval)
+    }
     
-    if(pval_sales < 0.05 & pval_gdp_j < 0.05) {
+    if(pval < 0.05) {
       cat("\n")
       cat(":::", st, "is significant for the year of", year)
       cat("\n")
     }
   }
 }
-in
+saveRDS(mundalkOut_dest, file = "diagnostics/mundlakOut_dest.rds")
+saveRDS(mundalkOut_orig, file = "diagnostics/mundlakOut_orig.rds")
+#end
